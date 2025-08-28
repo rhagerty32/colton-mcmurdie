9236a36a0db3da9230ad619ce3ad7947b667d150
rhagerty32 <ry2402@gmail.com>
2025-08-27 19:58:41 -0600

    adjust tag?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -22,36 +23,42 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <head>
-                <script
-                    type="text/javascript"
+            <body
+                className={`${inter.variable} font-body bg-background text-text antialiased`}
+            >
+                {/* Microsoft Clarity */}
+                <Script
+                    id="microsoft-clarity"
+                    strategy="beforeInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `
                     (function(c,l,a,r,i,t,y){
-                        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "t1nraym8x8");
+                    })(window,document,"clarity","script","t1nraym8x8");
                 `
                     }}
                 />
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-PRM5PKH9LL"></script>
-                <script
-                    type="text/javascript"
+
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-PRM5PKH9LL"
+                    strategy="afterInteractive"
+                />
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
-
                             gtag('config', 'G-PRM5PKH9LL');
                         `
                     }}
                 />
-            </head>
-            <body
-                className={`${inter.variable} font-body bg-background text-text antialiased`}
-            >
+
                 {children}
             </body>
         </html>
